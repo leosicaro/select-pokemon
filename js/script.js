3d570266ce69ce453de4c561ee04e7d30beabc1c
Leonel Sicaro <leonelsicaro@gmail.com>
2023-11-19 20:32:24 +0100

    pokemon

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-const bulbasaur  = document.getElementsByTagName("bulbasaur")
-const charmander = document.getElementsByTagName("charmander")
-const squirtle   = document.getElementsByTagName("squirtle");
-const button     = document.getElementById("get-pokemon");
-const container  = document.getElementById("container")
+
+const button            = document.getElementById("get-pokemon");
+const container         = document.getElementById("container")
+const pokemon_select    = document.getElementById("pokemon-select")
+let pokemonSeleccionado = pokemon_select.value;
+console.log(pokemonSeleccionado)
+
 
 button.addEventListener("click",()=>{
-    fetch(`https://pokeapi.co/api/v2/pokemon/charmander`)
+   
+   pokemonSeleccionado = pokemon_select.value;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonSeleccionado}`)
  .then((response)=>{
         if (!response.ok){
             throw new Error("La solicitud no fue exitosa");
@@ -13,21 +17,21 @@ button.addEventListener("click",()=>{
         return response.json()
      })
 .then((data) => {
-        let datosPokemon = data
-        datosPokemon.forEach(datoPokemon => {
-    
-           let listItem= `
-          <li> 
-          <p><span>Nombre: </span>${datoPokemon.name}</p>
-          <p>  <img src="${datoPokemon.sprites.front_default}" alt='${datoPokemon.name}' />
-          <p><span> <height:<span>${datoPokemon.height}</p>
-          <p><span> <weight:<span>${datoPokemon.weight}</p>
-          <p><span>Especie: </span>${datoPokemon.species}</p>
-        </li>`
-        container.innerHTML = listItem
-        });
-   })
-});
+        let datosPokemon = [data]
+        console.log(datosPokemon)
+        for (const datoPokemon of datosPokemon) {
+         let listItem = `
+         <li>
+         <p><span>Nombre: </span>${datoPokemon.name}</p>
+         <p> <img src="${datoPokemon.sprites.front_default}" alt='${datoPokemon.name}' />
+         <p><span> <height:<span>${datoPokemon.height}</p>
+         <p><span> <weight:<span>${datoPokemon.weight}</p>
+         <p><span>Tipo: </span>${datoPokemon.types?.[0]?.type?.name}</p>
+         </li>`;
+       
+         container.innerHTML = listItem;}});
+        })
+
      
 
 
